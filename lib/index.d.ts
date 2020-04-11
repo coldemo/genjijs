ee8cf946fc5ef219c8add17bebe37d660effb803
Fritz Lin <uxfritz@163.com>
2020-04-11 22:01:35 +0800

    🔧 npm run build & npm publish

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,63 +1,58 @@
-export declare type Indexable<T extends {}> = {
-  [key: string]: T;
+export declare type Indexable<T> = {
+    [key: string]: T;
 };
-
 export interface GenjiAction {
-  type: string;
-  payload: Object;
+    type: string;
+    payload: Object;
 }
-
 export interface GenjiOperations {
-  dispatch?: Function;
-  getState?: Function;
-  pick: GenjiPick;
-  save: GenjiSave;
+    dispatch?: Function;
+    getState?: Function;
+    pick: GenjiPick;
+    save: GenjiSave;
 }
-
 export declare type GenjiSave = (updateState: Object, assignNamespace?: string) => void;
 export declare type GenjiPick = (stateKey?: string[] | string, assignNamespace?: string) => unknown;
-
-export declare type GenjiDispatch = (action: { type: string; payload?: Object }) => Promise<unknown>;
+export declare type GenjiDispatch = (action: {
+    type: string;
+    payload?: Object;
+}) => Promise<unknown>;
 export declare type GenjiActionCreator = (action: GenjiAction, operations: GenjiOperations) => unknown;
 export declare type GenjiActionCreatorObj = {
-  type: string;
-  actionCreator: GenjiActionCreator;
+    type: string;
+    actionCreator: GenjiActionCreator;
 };
 export declare type GenjiReducer = (state: unknown, action: GenjiAction) => {};
 export interface GenjiActionCreators {
-  [key: string]: GenjiActionCreator;
+    [key: string]: GenjiActionCreator;
 }
 export interface GenjiCommonReducers {
-  [key: string]: Function;
+    [key: string]: Function;
 }
 export interface GenjiModel {
-  namespace: string;
-  state: {
-    [key: string]: unknown;
-  };
-  actionCreators: GenjiActionCreators;
-  reducers?: GenjiCommonReducers;
+    namespace: string;
+    state: Indexable<unknown>;
+    actionCreators: GenjiActionCreators;
+    reducers?: GenjiCommonReducers;
 }
-export interface States {
-  [key: string]: unknown;
+export interface RootStates {
+    [namespace: string]: Object;
 }
 export interface GenjiConfig {
-  injectAsyncLoading?: boolean;
-  autoUpdateAsyncLoading?: boolean;
+    injectAsyncLoading?: boolean;
+    autoUpdateAsyncLoading?: boolean;
 }
 declare class Genji {
-  _models: GenjiModel[];
-  _store: Object;
-  _states: States;
-  _reducers: GenjiCommonReducers;
-  _actionCreatorObjs: GenjiActionCreatorObj[];
-  config: GenjiConfig;
-  _reducersTree: {
-    [key: string]: GenjiReducer;
-  };
-  constructor(config?: {});
-  model<T>(model: GenjiModel): { [key in keyof T]?: string };
-  start(): void;
-  getStore(): Object;
+    _models: GenjiModel[];
+    _store: any;
+    _states: RootStates;
+    _reducers: GenjiCommonReducers;
+    _actionCreatorObjs: GenjiActionCreatorObj[];
+    config: GenjiConfig;
+    _reducersTree: Indexable<GenjiReducer>;
+    constructor(config?: {});
+    model<T>(model: GenjiModel): { [key in keyof T]?: string; };
+    start(): void;
+    getStore(): any;
 }
 export default Genji;
